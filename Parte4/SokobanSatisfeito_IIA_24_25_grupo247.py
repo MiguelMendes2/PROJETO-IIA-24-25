1f81c2c734a9cf5e3cf4f6b14101c2c2a8c8d497
mads-on-code <madalenafmachado@gmail.com>
2024-12-16 03:22:59 +0000

    ja printa bem e passa +1 teste (5.88/20)

--- Parte4/SokobanSatisfeito_IIA_24_25_grupo247.py
+++ Parte4/SokobanSatisfeito_IIA_24_25_grupo247.py
@@ -1,26 +1,60 @@
-from csp_v3 import *
+# Nossas funcoes
+def csp_possivel_solucao(caixas, goals_alcancaveis):
+    variables = list(caixas)
+    domains = {var: goals_alcancaveis[var] for var in variables}
+    neighbors = {var: [var2 for var2 in variables if var != var2 and set(goals_alcancaveis[var]).intersection(goals_alcancaveis[var2]) and goals_alcancaveis[var2]] for var in variables}
+    for var in variables:
+        if neighbors[var] == []:
+            neighbors.pop(var)
+    def constraints(var1, a, var2, b):
+        return a != b
 
-def csp_possivel_solucao(s):
-    for i in range(0, len(s.boxes)):
-        if s.boxes[i] not in s.goal:
-            return False
-    return True
+    return CSP(variables, domains, neighbors, constraints)
 
 def csp_find_alcancaveis_1goal(s, goal):
-    alcancaveis = set()
-    alcancaveis.add(goal)
-    for i in range(0, len(s.boxes)):
-        if s.boxes[i] == goal:
-            for j in range(0, len(s.boxes)):
-                if i != j:
-                    if (s.boxes[j][0] == s.boxes[i][0] and abs(s.boxes[j][1] - s.boxes[i][1]) == 1) or (s.boxes[j][1] == s.boxes[i][1] and abs(s.boxes[j][0] - s.boxes[i][0]) == 1):
-                        alcancaveis.add(s.boxes[j])
-    return alcancaveis
+    caixas = s.initial['caixas']
+    variables = list(s.navegaveis)
+    domains = {var: [0, 1] for var in variables}
+    neighbors = {var1: [var2 for var2 in variables if var1 != var2 and eh_vizinho(var1, var2, s, caixas)] for var1 in variables}
 
 
+    def constraints(var1, a, var2, b):
+        if var2 not in neighbors[var1]:
+            return True
+        if a == 1 and b == 0:
+            return False
+        if b == 1 and a == 0:
+            return False
+        return True
+
+    return CSP(variables, domains, neighbors, constraints)
+
+def eh_vizinho(celula1, celula2, s, caixas):
+    diff_linha = celula2[0] - celula1[0]
+    diff_coluna = celula2[1] - celula1[1]
+    deltas = diff_linha, diff_coluna
+
+    if s.possible(s.initial['sokoban'], caixas, deltas) and (abs(diff_linha) + abs(diff_coluna) == 1):
+        return True
+
+    return False
+    
 def find_alcancaveis_all_goals(s):
     sorted_goals = sorted(list(s.goal))
     result_alcancaveis = {}
+    
     for goal in sorted_goals:
-        result_alcancaveis[goal] = csp_find_alcancaveis_1goal(s, goal)
+        csp_sokoban2 = csp_find_alcancaveis_1goal(s, goal)
+        r = backtracking_search(csp_sokoban2, order_domain_values=number_ascending_order, inference=forward_checking)
+        if r is not None:
+            for cell, reachable in r.items():
+                if cell not in result_alcancaveis:
+                    result_alcancaveis[cell] = []
+                if reachable == 1:
+                    result_alcancaveis[cell].append(goal)
+
+    for cell in s.navegaveis:
+        if cell not in result_alcancaveis:
+            result_alcancaveis[cell] = []
+
     return result_alcancaveis
